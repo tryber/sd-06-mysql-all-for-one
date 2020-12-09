f77ff3965c9eab14f389b090d350053dd87d39a0
Stefano <stefano.branz@gmail.com>
2020-12-09 18:54:39 -0300

    Requisite 12 - push 01

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,1 @@
-SELECT notes
-FROM northwind.purchase_orders
-WHERE notes IS NOT NULL
-ORDER BY notes
-LIMIT 0, 6;
+SELECT notes FROM northwind.purchase_orders WHERE notes IS NOT NULL ORDER BY notes LIMIT 0, 6;
