c4382036d2cbcac0681467e549e84d6edb7a65a4
ricknois <rickssti@outlook.com>
2020-12-09 17:17:23 -0300

    :sparkles: Desafios #20 a 24

--- tests/tableManipulationChallenges.spec.js
+++ tests/tableManipulationChallenges.spec.js
@@ -2,7 +2,7 @@ const { readFileSync } = require('fs');
 const { Sequelize } = require('sequelize');
 const Importer = require('mysql-import');
 
-describe.skip('Desafios de manipulação de tabelas', () => {
+describe('Desafios de manipulação de tabelas', () => {
   let importer;
   let sequelize;
 
@@ -28,7 +28,7 @@ describe.skip('Desafios de manipulação de tabelas', () => {
 
   afterEach(async () => await sequelize.query('DROP DATABASE northwind;', { type: 'RAW' }));
 
-  describe.skip('Queries de inserção', () => {
+  describe('Queries de inserção', () => {
     const countOrderDetailsQuery = `SELECT COUNT(*) AS details_count FROM northwind.order_details
       WHERE order_id = 69
             AND product_id = 80
@@ -43,7 +43,7 @@ describe.skip('Desafios de manipulação de tabelas', () => {
     const lastOrderDetailsIdsQuery = (limit = 1) =>
       `SELECT id FROM northwind.order_details ORDER BY id DESC LIMIT ${limit};`;
 
-    describe.skip('Adicione ao `order_details` uma linha com os seguintes dados: `order_id`: 69, `product_id`: 80, `quantity`: 15.0000, `unit_price`: 15.0000, `discount`: 0, `status_id`: 2, `date_allocated`: NULL, `purchase_order_id`: NULL e `inventory_id`: 129', () => {
+    describe('Adicione ao `order_details` uma linha com os seguintes dados: `order_id`: 69, `product_id`: 80, `quantity`: 15.0000, `unit_price`: 15.0000, `discount`: 0, `status_id`: 2, `date_allocated`: NULL, `purchase_order_id`: NULL e `inventory_id`: 129', () => {
       it('Verifica o desafio20', async () => {
         const challengeQuery = readFileSync('desafio20.sql', 'utf8').trim();
         const lastOrderDetailsId = (
@@ -63,7 +63,7 @@ describe.skip('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe.skip('Adicione, com um único `INSERT`, duas linhas ao `order_details` com os mesmos dados. Esses dados são novamente os mesmos do requisito 20', () => {
+    describe('Adicione, com um único `INSERT`, duas linhas ao `order_details` com os mesmos dados. Esses dados são novamente os mesmos do requisito 20', () => {
       it('Verifica o desafio21', async () => {
         const challengeQuery = readFileSync('desafio21.sql', 'utf8').trim();
         const lastOrderDetailsId = (
@@ -84,11 +84,11 @@ describe.skip('Desafios de manipulação de tabelas', () => {
     });
   });
 
-  describe.skip('Queries de atualização', () => {
+  describe('Queries de atualização', () => {
     const countOrderDetailsByDiscountQuery = (discount) =>
       `SELECT COUNT(*) AS details_count FROM order_details WHERE discount = ${discount};`;
 
-    describe.skip('Atualize os dados de `discount` do `order_details` para 15', () => {
+    describe('Atualize os dados de `discount` do `order_details` para 15', () => {
       it('Verifica o desafio22', async () => {
         const challengeQuery = readFileSync('desafio22.sql', 'utf8').trim();
 
@@ -102,7 +102,7 @@ describe.skip('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe.skip('Atualize os dados de `discount` da tabela `order_details` para 30 cuja `unit_price` seja menor que 10.0000', () => {
+    describe('Atualize os dados de `discount` da tabela `order_details` para 30 cuja `unit_price` seja menor que 10.0000', () => {
       it('Verifica o desafio23', async () => {
         const challengeQuery = readFileSync('desafio23.sql', 'utf8').trim();
 
@@ -116,7 +116,7 @@ describe.skip('Desafios de manipulação de tabelas', () => {
       });
     });
 
-    describe.skip('Atualize os dados de `discount` da tabela `order_details` para 45 cuja `unit_price` seja maior que 10.0000 e o id seja um número entre 30 a 40', () => {
+    describe('Atualize os dados de `discount` da tabela `order_details` para 45 cuja `unit_price` seja maior que 10.0000 e o id seja um número entre 30 a 40', () => {
       it('Verifica o desafio24', async () => {
         const challengeQuery = readFileSync('desafio24.sql', 'utf8').trim();
 
