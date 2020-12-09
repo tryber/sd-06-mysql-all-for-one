4b4633a7868b7f504f4fbb1ff3c5f5b86df2279b
Rods <rods.leite27@gmail.com>
2020-12-09 19:35:05 -0300

    req 21

--- desafio20.sql
+++ desafio20.sql
@@ -1,4 +1,4 @@
-INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+INSERT INTO order_details.northwind (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
 VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
 SELECT * from northwind.order_details
 ORDER BY id ASC
