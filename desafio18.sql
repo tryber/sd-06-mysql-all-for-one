4f2e48cadfcb3cee323c0726e2f20eb01faf4c14
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-12-09 18:31:06 -0300

    req16-19

---
+++ desafio18.sql
@@ -0,0 +1,3 @@
+SELECT * FROM northwind.purchase_orders
+WHERE supplier_id = 3
+OR status_id = 2;
---
+++ desafio19.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(id) FROM northwind.orders
+WHERE employee_id = 5
+OR employee_id = 6
+AND shipper_id = 2;
