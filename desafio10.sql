3b8a5dbaef8c257669d3af06ae79b91097250304
afornagieri <fornagieri.artur@gmail.com>
2020-12-09 15:15:10 -0300

    Resolvidos problemas com desafios 10 e 17

--- desafio10.sql
+++ desafio10.sql
@@ -1,2 +1,2 @@
-SELECT purchase_orders.created_by FROM northwind.purchase_orders WHERE
-purchase_orders.created_by >= 3 ORDER BY id ASC;
+SELECT * FROM northwind.purchase_orders WHERE purchase_orders.created_by >= 3
+ORDER BY created_by DESC, id ASC;
