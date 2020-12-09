29c8e987f914a7910d0025c4a675d1ad24286cdd
luiz <phellipe501@gmail.com>
2020-12-09 16:55:48 -0300

    fix cc

--- desafio1.sql
+++ desafio1.sql
@@ -13,18 +13,9 @@ SELECT COUNT(product_name) FROM northwind.products;
 SELECT * FROM northwind.products LIMIT 10 OFFSET 3;
 
 -- 6
-SELECT product_name, id FROM northwind.products
-ORDER BY product_name ASC;
+SELECT product_name, id FROM northwind.products ORDER BY product_name ASC;
 
 -- 7
-SELECT id FROM northwind.products
-ORDER BY id DESC LIMIT 5;
+SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5;
 
 -- 8
- 
-
-
-
-
-
-
