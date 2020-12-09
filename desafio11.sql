d7eff0ead58e04e9aebfcbd5eaa98d44b7a1e3eb
Ana Flávia <anaflvia@gmail.com>
2020-12-09 15:56:22 -0300

    Requisitos 10, 11 e 12

---
+++ desafio11.sql
@@ -0,0 +1,11 @@
+SELECT * FROM northwind.purchase_orders
+WHERE notes='Purchase generated based on Order #30'
+OR notes='Purchase generated based on Order #31'
+OR notes='Purchase generated based on Order #32'
+OR notes='Purchase generated based on Order #33'
+OR notes='Purchase generated based on Order #34'
+OR notes='Purchase generated based on Order #35'
+OR notes='Purchase generated based on Order #36'
+OR notes='Purchase generated based on Order #37'
+OR notes='Purchase generated based on Order #38'
+OR notes='Purchase generated based on Order #39';
