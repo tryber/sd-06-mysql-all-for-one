48ecfeb062799171f176909219af7b30502f0349
Paulo <paulocheloni@gmail.com>
2020-12-10 16:21:47 -0300

    fixing lint

--- desafio20.sql
+++ desafio20.sql
@@ -3,7 +3,7 @@ INSERT INTO northwind.order_details(order_id,
     quantity,
       unit_price,
         discount,
-         status_id,
-           inventory_id,
-             date_allocated, purchase_order_id)
+          status_id,
+            inventory_id,
+              date_allocated, purchase_order_id)
   VALUES(69, 80, 15.0000, 15.0000, 0, 2 ,129, NULL, NULL);
