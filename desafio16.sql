41abe1d88f15d166d12df7e257c394b005f7938e
rafMewtwo <ponciano.raf@gmail.com>
2020-12-10 13:11:20 -0300

    ready and gooo!x002

--- desafio16.sql
+++ desafio16.sql
@@ -1,2 +1,2 @@
 SELECT submitted_date FROM northwind.purchase_orders
-WHERE YEAR(submitted_date) = 2006 AND MONTH(submitted_date) BETWEEN 1 AND 3;
+WHERE YEAR(submitted_date) = 2006 AND DAY(submitted_date) >= 26 AND MONTH(submitted_date) BETWEEN 1 AND 3;
