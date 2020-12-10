9a6b22c494d900a46412517d90bc4f6bfd759442
Leandro <leandro.feitoza8018@gmail.com>
2020-12-10 13:08:01 -0300

    correçao da formatacao - CC

--- desafio17.sql
+++ desafio17.sql
@@ -1,2 +1,8 @@
-SELECT id, supplier_id FROM northwind.purchase_orders
-	WHERE supplier_id = 1 OR supplier_id = 3 OR supplier_id = 5 OR supplier_id = 7;
+SELECT 
+    id, supplier_id
+FROM
+    northwind.purchase_orders
+WHERE
+    supplier_id = 1 OR supplier_id = 3
+        OR supplier_id = 5
+        OR supplier_id = 7;
