6b634a70889c4c50658805d3a114cc9b930df4b6
Leandro <leandro.feitoza8018@gmail.com>
2020-12-10 13:18:39 -0300

    desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,1 +1,6 @@
-SELECT creation_date FROM purchase_orders WHERE submitted_date LIKE '%2006-04-26%';
+SELECT 
+    creation_date
+FROM
+    northwind.purchase_orders
+WHERE
+    submitted_date LIKE '%2006-04-26%';
