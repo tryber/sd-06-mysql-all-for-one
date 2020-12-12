2482a7446b3cc9e6e04f30c81513a593f9bbc221
pa-volpin <paulo.volpin@gmail.com>
2020-12-12 18:58:49 -0300

    Corrections based on Codeclimate

--- desafio21.sql
+++ desafio21.sql
@@ -9,3 +9,4 @@ INSERT INTO northwind.order_details(
   purchase_order_id,
   inventory_id)
   VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129), (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
+  
