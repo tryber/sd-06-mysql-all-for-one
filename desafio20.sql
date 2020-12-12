215af9a118f528e69426c4cf79c5bd2622fd14b9
pa-volpin <paulo.volpin@gmail.com>
2020-12-12 19:11:48 -0300

    Corrections based on Codeclimate

--- desafio20.sql
+++ desafio20.sql
@@ -8,7 +8,4 @@ INSERT INTO northwind.order_details(
   date_allocated,
   purchase_order_id,
   inventory_id)
-  VALUES (
-    69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129
-  );
-  
+  VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
--- desafio21.sql
+++ desafio21.sql
@@ -9,4 +9,3 @@ INSERT INTO northwind.order_details(
   purchase_order_id,
   inventory_id)
   VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129), (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
-  
