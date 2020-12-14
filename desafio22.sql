16e50153083ced0835bccfc005aaf419cd473008
FlavioSugano <flavio.sugano@gmail.com>
2020-12-14 18:11:10 -0300

    Desafio 22

--- desafio22.sql
+++ desafio22.sql
@@ -1,5 +1,5 @@
 -- Atualize os dados de discount do order_details para 15.  
-select * from northwind.order_details order by discount desc;
-update northwind.order_details
-set discount = 15
-where discount = 0;
+SELECT * FROM northwind.order_details ORDER BY discount DESC;
+UPDATE northwind.order_details
+SET discount = 15
+WHERE id > 0;
