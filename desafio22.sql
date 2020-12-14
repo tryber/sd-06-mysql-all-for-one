6c1a2240c29dfd753aaadbc548c78eaa25627e57
FlavioSugano <flavio.sugano@gmail.com>
2020-12-14 18:26:42 -0300

    Desafio 22: remoçao de codigo nao utilizado

--- desafio22.sql
+++ desafio22.sql
@@ -1,5 +1,4 @@
--- Atualize os dados de discount do order_details para 15.  
-SELECT * FROM northwind.order_details ORDER BY discount DESC;
+-- Atualize os dados de discount do order_details para 15.
 UPDATE northwind.order_details
 SET discount = 15
 WHERE id > 0;
