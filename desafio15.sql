e953539dd6349d1d2cbd7e12bf59f852063e769f
fomes <contato.felipeluna@gmail.com>
2020-12-23 20:54:15 -0300

    finish project

---
+++ desafio15.sql
@@ -0,0 +1,1 @@
+SELECT SUBSTRING(submitted_date, 15, 2) AS 'submitted_hour' FROM northwind.purchase_orders;
---
+++ desafio21.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+	northwind.order_details
+VALUES
+	(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129'),
+	(DEFAULT, '69', '80', '15.0000', '15.0000', '0', '2', NULL, NULL, '129');
