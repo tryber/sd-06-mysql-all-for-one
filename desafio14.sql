d67f32af8f7721c6fff21948cf4fdb4d1e1efbdf
higoramorim <1810042@aluno.univesp.br>
2020-12-27 14:28:46 -0300

    14,15 e 19

--- desafio14.sql
+++ desafio14.sql
@@ -1,1 +1,1 @@
-SELECT * FROM northwind.purchase_orders WHERE supplier_id BETWEEN 1 AND 3 ORDER BY supplier_id;
+SELECT supplier_id FROM northwind.purchase_orders WHERE supplier_id BETWEEN 1 AND 3 ORDER BY supplier_id;
