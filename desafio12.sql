6e6170ee7f6c19326ef00406344197e569fc0964
PAULO LINS <paulolins100@gmail.com>
2021-01-05 16:18:35 -0300

    desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,2 +1,3 @@
-select notes, submitted_date from northwind.purchase_orders
-where date(submitted_date) = '2006-04-26';
+SELECT submitted_date FROM northwind.purchase_orders
+where submitted_date 
+between '2006-04-26 00:00:00' and '2006-04-26 23:59:59';
